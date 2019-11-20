9b13ad8cffc6a3288e5a3b21885a1341a5189991
haytamkies11 <49904030+haytamkies11@users.noreply.github.com>
2019-11-20 14:14:25 +0200

    Update install_commands.sh

--- install_commands.sh
+++ install_commands.sh
@@ -1,5 +1,3 @@
-apt-get update
-apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys 3FA7E0328081BFF6A14DA29AA6A19B38D3D831EF
 apt-get install mono-complete
-wget https://dist.nuget.org/win-x86-commandline/latest/nuget.exe
-nuget.exe install
+
+mono nuget.exe install
