057b3300f49b039bf50f7bd797836f5634f6fc32
haytamkies11 <49904030+haytamkies11@users.noreply.github.com>
2019-11-20 14:22:16 +0200

    Update install_commands.sh

--- install_commands.sh
+++ install_commands.sh
@@ -1,5 +1,6 @@
 apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys 3FA7E0328081BFF6A14DA29AA6A19B38D3D831EF
 "deb http://download.mono-project.com/repo/debian wheezy main" | sudo tee /etc/apt/sources.list.d/mono-xamarin.list
+apt-get -y install git autoconf libtool g++ gettext make mono-complete
 apt-get update
 apt-get install mono-complete
 mono nuget.exe install
